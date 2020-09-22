f61c7fb2cc79ed700201cec71b1cf17b74322dc7
AndresMontealegre <55224609+AndresMontealegre@users.noreply.github.com>
2020-09-21 20:39:30 -0500

    asd
    asd

--- front/src/components/ProyectsComponent.vue
+++ front/src/components/ProyectsComponent.vue
@@ -4,7 +4,236 @@
             <div class="col-1">
             </div>
             <div class="col-10 container">
-             <div class="text-h6">hola mundo</div>
+                <div class="title">
+                    <div class="text-h6">
+                        Usuarios
+                        <q-btn round color="positive" size="sm" icon="add">
+                            <q-tooltip>
+                                Agregar
+                            </q-tooltip>
+                        </q-btn>
+                    </div>
+                </div>
+                <q-separator />
+                <br>
+                <div class="row">
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                    <div class="col-6">
+                        <q-card class="my-card" flat bordered>
+                            <q-card-section horizontal>
+                                <q-card-section class="cards">
+                                Lorem
+                                </q-card-section>
+
+                                <q-img
+                                class="col-5"
+                                src="https://cdn.quasar.dev/img/parallax2.jpg"
+                                />
+                            </q-card-section>
+                            <q-separator />
+
+                            <q-card-actions>
+                                <q-btn flat color="primary">
+                                Ver
+                                </q-btn>
+                                <q-btn flat color="primary">
+                                Editar
+                                </q-btn>
+                                <q-btn flat color="negative">
+                                Eliminar
+                                </q-btn>
+                            </q-card-actions>
+                        </q-card>
+                    </div>
+                </div>
             </div>
             <div class="col-1"></div>
         </div>
