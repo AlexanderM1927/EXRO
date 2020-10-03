4b1d70116dceef5a3034e42f0e9d304532b74fda
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2020-10-03 16:07:35 -0500

    xxx

---
+++ back/app/Http/Controllers/InstagramController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Log;
+
+class InstagramController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function curl_instagram( $api_url ){
+        $ans = file_get_contents($api_url);
+        return json_decode($ans);
+    }
+
+    public function getPosts() {
+        $access_token = 'IGQVJVSnRuVjRfRGdNaDlyMHJLbXkzYXFOMEV0YVdhMlZAXcWZAWT1VvY0wxSG9HX1YxSHZABLVVpdl9nQTJYelI3UmtzQnoycm9WZAC16cGE3a09uNkF6WXRYNng3bGszd0VUcFFYa3doc0ZAtLXBlMHljdwZDZD';
+        $user_search = $this->curl_instagram('https://graph.instagram.com/me/media?fields=id,caption,media_url&access_token='. $access_token);
+        // echo $user_search;
+        foreach($user_search->data as $post) {
+            echo "<img src='".$post->media_url."'>";
+            echo "<br><br>";
+        }
+        // return response()->json(['ans' => $user_search]);
+    }
+
+    //
+}
