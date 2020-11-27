e0621d3793ab4904a07542eec3200ffedb67c56d
AlexanderM1927 <52513621+AlexanderM1927@users.noreply.github.com>
2020-11-26 21:49:47 -0500

    icons

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -130,31 +130,41 @@ module.exports = function (/* ctx */) {
         theme_color: '#027be3',
         icons: [
           {
-            src: 'icons/icon-128x128.png',
+            src: 'logo.png',
             sizes: '128x128',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-192x192.png',
+            src: 'logo.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-256x256.png',
+            src: 'logo.png',
             sizes: '256x256',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-384x384.png',
+            src: 'logo.png',
             sizes: '384x384',
             type: 'image/png'
           },
           {
-            src: 'icons/icon-512x512.png',
+            src: 'logo.png',
             sizes: '512x512',
             type: 'image/png'
           }
         ]
+      },
+      metaVariables: {
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black-translucent',
+        appleTouchIcon120: 'logo.png',
+        appleTouchIcon180: 'logo.png',
+        appleTouchIcon152: 'logo.png',
+        appleTouchIcon167: 'logo.png',
+        msapplicationTileImage: 'logo.png',
+        msapplicationTileColor: '#000000'
       }
     },
 
