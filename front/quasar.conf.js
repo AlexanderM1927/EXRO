a22a9b511d0d17dfc16ff1cf84ace8efd7b141b0
John A. Martinez <52513621+AlexanderM1927@users.noreply.github.com>
2024-03-11 16:52:13 -0500

    Dockerize project with Lumen and Quasar 1.0

--- front/quasar.conf.js
+++ front/quasar.conf.js
@@ -7,7 +7,6 @@
 // https://quasar.dev/quasar-cli/quasar-conf-js
 /* eslint-env node */
 const DotEnv = require('dotenv')
-const Webpack = require('Webpack')
 // get out env variables
 const envparsers = require('./envparser')
 const envparser = require('./envparser')
@@ -81,7 +80,7 @@ module.exports = function (/* ctx */) {
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-devServer
     devServer: {
       https: false,
-      port: 8080,
+      port: 9090,
       open: false // opens browser window automatically
     },
 
